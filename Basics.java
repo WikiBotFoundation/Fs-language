90b229653a4dfdd0fcafbb2bb238ad50089ba4d8
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-03 23:04:30 +0300

    Basics

---
+++ Basics.java
@@ -0,0 +1,17 @@
+<script>
+  componets
+  @Owerride 
+      File="structure-ruby.load)"
+    Componets.constr(java
+         Load.ruby.load(java.programming == script
+
+  @Owerride
+     Components.java == jar load.ruby
+     Basic.load == java.jar. == programming
+            Load. == JAVA 
+   @Owerride
+     Load.java.ru(jar. == Load.componets
+      LEXER float.jar
+            @Owerride
+      Load.Componets == java. Load == ruby
+        JAVA == script.file
