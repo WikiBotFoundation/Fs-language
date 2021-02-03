5e4711ac085bf077a4b6400861c8f15f83a09a03
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-03 22:54:25 +0300

    Create Language.rb

---
+++ MakeJava/Language.rb
@@ -0,0 +1,7 @@
+<script>
+     ruby:load.(load[sc.on1(2.1)
+          Load.script(programmimg.rub(data(
+    [1.0./load.programconsole.82[1.0]
+        Java @ruby == language
+             Owerride.load[1.20.0]
+
