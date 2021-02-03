64ad2b347bb382b7d7eb81e9b7c33361e0b329e8
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-03 23:18:13 +0300

    Create Talk.m

---
+++ Components/Talk.m
@@ -0,0 +1,10 @@
+<shell> 
+    [[Main.class == Objecive
+       { Class.main
+     Talk.cm(ls
+       [Objective-cm
+            Class.main.cs(file)-Objective
+       == Token ==
+       { Talks == main.cs(file.ob(file.ob
+           [ Main.class.j
+                 
