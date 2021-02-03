9f7ac78e95deeba75718a06a6b43bcb8fd2d9d6c
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-03 22:47:03 +0300

    Create Class.cs

---
+++ Class.cs
@@ -0,0 +1,12 @@
+<script>
+  View(.import(cs)
+     ProgrammingLanguage(cs.makefile
+
+     public  class="cs.machine"
+         File.py(class.med)
+          
+    Public void(classname.med(machine.file)
+          PyMed.(class.extensions(java.j
+              MakeFile.type(extensions.mj
+      Extensions.cs(builder.js(med.j)
+            
